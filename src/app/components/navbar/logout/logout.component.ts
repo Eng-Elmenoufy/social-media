01380838ad029794b535a2906ab09c60d9af5776
Eng-Elmenoufy <mahmoudmo7elmenoufy@gmail.com>
2024-12-31 18:36:23 +0200

    Implemented logout form with applied styling.

--- src/app/components/navbar/logout/logout.component.ts
+++ src/app/components/navbar/logout/logout.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-logout',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+  ],
   templateUrl: './logout.component.html',
-  styleUrl: './logout.component.scss'
+  styleUrl: './logout.component.scss',
 })
 export class LogoutComponent {
-
+  private auth = inject(AuthService);
+  onLogout() {
+    this.auth.logout();
+  }
 }
